db8b1598af5bc5a46301e5b85233884fd0144d7b
Ramesh Khairandish <r.khairandish@dundee.ac.uk>
2017-10-05 16:15:08 +0100

    Initial commit

---
+++ HelloRamesh.cpp
@@ -0,0 +1,15 @@
+//  HelloRamesh.cpp
+//  Workspace
+//  Created by ramesh khairandish on 05/10/2017.
+//  Copyright © 2017 ramesh khairandish. All rights reserved.
+
+#include <cstdio>
+#include <iostream>
+using namespace std;
+
+int main( int argc, char ** argv )
+{
+    cout << "Hello, Ramesh!\n";
+    return 0;
+}
+
